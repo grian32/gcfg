fc8ef6e93cafbeb663ae8bffdc7a404569275107
grian32 <grianguy32@gmail.com>
2025-08-15 02:57:44 +0300

    feat: lex single char tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -7,6 +7,17 @@ type Lexer struct {
 	ch      byte
 }
 
+var singleCharTokens = map[byte]TokenType{
+	'[': LBRACKET,
+	']': RBRACKET,
+	'(': LPAREN,
+	')': RPAREN,
+	'{': LBRACE,
+	'}': RBRACE,
+	'=': ASSIGN,
+	',': COMMA,
+}
+
 func New(input []byte) *Lexer {
 	l := &Lexer{input: input}
 	l.advance()
@@ -33,5 +44,38 @@ func (l *Lexer) peek() byte {
 }
 
 func (l *Lexer) NextToken() Token {
-	return Token{}
+	var tok Token
+
+	// TODO: consider not doing this.. maybe just for indentation
+	l.skipWhitespace()
+
+	singleTok, exists := singleCharTokens[l.ch]
+	if exists {
+		tok = newSingleToken(singleTok, l.ch)
+		l.advance()
+		return tok
+	}
+
+	if l.ch == 0 {
+		tok.Literal = ""
+		tok.Type = EOF
+	} else if l.ch == '"' {
+		// strings
+	} else if IsDigit(l.ch) {
+		// ints and floats
+	} else {
+		// identifiers, booleans, null
+	}
+
+	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.advance()
+	}
+}
+
+func newSingleToken(tokType TokenType, ch byte) Token {
+	return Token{Type: tokType, Literal: string(ch)}
 }
---
+++ lexer/util.go
@@ -0,0 +1,5 @@
+package lexer
+
+func IsDigit(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
