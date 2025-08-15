f5ca891200910cf1b2ff19d82b0f21b510dafda8
grian32 <grianguy32@gmail.com>
2025-08-15 03:32:28 +0300

    feat: lex ints/floats

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,9 +1,11 @@
 package lexer
 
+import "errors"
+
 type Lexer struct {
 	input   []byte
 	readPos int
-	prevPos int
+	pos     int
 	ch      byte
 }
 
@@ -31,7 +33,7 @@ func (l *Lexer) advance() {
 		l.ch = l.input[l.readPos]
 	}
 
-	l.prevPos = l.readPos
+	l.pos = l.readPos
 	l.readPos += 1
 }
 
@@ -43,7 +45,7 @@ func (l *Lexer) peek() byte {
 	}
 }
 
-func (l *Lexer) NextToken() Token {
+func (l *Lexer) NextToken() (Token, error) {
 	var tok Token
 
 	// TODO: consider not doing this.. maybe just for indentation
@@ -53,7 +55,7 @@ func (l *Lexer) NextToken() Token {
 	if exists {
 		tok = newSingleToken(singleTok, l.ch)
 		l.advance()
-		return tok
+		return tok, nil
 	}
 
 	if l.ch == 0 {
@@ -62,18 +64,43 @@ func (l *Lexer) NextToken() Token {
 	} else if l.ch == '"' {
 		// strings
 	} else if IsDigit(l.ch) {
-		// ints and floats
+		return l.readNumber()
 	} else {
 		// identifiers, booleans, null
 	}
 
-	return tok
+	return tok, nil
 }
 
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.advance()
 	}
+}
+
+func (l *Lexer) readNumber() (Token, error) {
+	startPos := l.pos
+	tokType := INT
+	float := false
+
+	for IsDigit(l.ch) || l.ch == '.' {
+		if l.ch == '.' {
+			if float {
+				return Token{}, errors.New("multiple dots not allowed in number")
+			}
+			float = true
+			tokType = FLOAT
+		}
+		l.advance()
+	}
+
+	literal := string(l.input[startPos:l.pos])
+
+	if literal[len(literal)-1] == '.' {
+		return Token{}, errors.New("numbers not allowed to end in dot")
+	}
+
+	return Token{Type: tokType, Literal: literal}, nil
 }
 
 func newSingleToken(tokType TokenType, ch byte) Token {
