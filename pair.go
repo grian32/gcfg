445d708b6727fbee8d10df96888933e2e27355f8
grian32 <grianguy32@gmail.com>
2025-08-16 04:21:55 +0300

    feat: pair sturct

---
+++ pair.go
@@ -0,0 +1,16 @@
+package gcfg
+
+import "fmt"
+
+type Pair[T, U any] struct {
+	First  T
+	Second U
+}
+
+func (p Pair[T, U]) String() string {
+	return fmt.Sprintf("(%v, %v)", p.First, p.Second)
+}
+
+func (p Pair[T, U]) Values() (T, U) {
+	return p.First, p.Second
+}
